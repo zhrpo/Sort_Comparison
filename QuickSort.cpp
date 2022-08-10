100b1719a72c16246a2aa31294f028a55fe26aea
Tmenzie <98368403+Tmenzie@users.noreply.github.com>
2022-08-09 23:09:44 -0500

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -4,21 +4,24 @@
 
 using namespace std;
 
-int QuickSort::split(int arr[], int first, int end) //swaps the data around to find the smallest value
+int QuickSort::split(int QuickSortArray[], int first, int end) //swaps the data around to find the smallest value
 {
 
-    int pivot = arr[first];
+    int pivot = QuickSortArray[first];
 
     int count = 0;
     for (int i = first + 1; i <= end; i++) 
     {
-        if (arr[i] <= pivot)
+        if (QuickSortArray[i] <= pivot)
+        {
             count++;
+        }
     }
 
 
+
     int pivotValue = first + count;
-    swap(arr[pivotValue], arr[first]);
+    swap(QuickSortArray[pivotValue], QuickSortArray[first]);
 
 
     int left = first;
@@ -28,41 +31,38 @@ int QuickSort::split(int arr[], int first, int end) //swaps the data around to f
     {
         if (left < pivotValue && right > pivotValue)
         {
-            swap(arr[left++], arr[right--]);
+            swap(QuickSortArray[left++], QuickSortArray[right--]);
         }
-        while (arr[left] <= pivot)
+        while (QuickSortArray[left] <= pivot)
         {
             left = left++;
         }
-
-        while (arr[right] > pivot)
+        while (QuickSortArray[right] > pivot)
         {
             right = right--;
-        }
-
-        
+        }   
     }
 
     return pivotValue;
 }
 
-void QuickSort::Sort(int arr[], int first, int end) //sorts data from the array
+void QuickSort::Sort(int QuickSortArray[], int first, int end) //sorts data from the array
 {
     if (first >= end)
     {
         return;
     }
 
-    int p = split(arr, first, end);//puls the first and last to swap out until it in numerical order
-    Sort(arr, first, p - 1);
-    Sort(arr, p + 1, end);
+    int p = split(QuickSortArray, first, end);//puls the first and last to swap out until it in numerical order
+    Sort(QuickSortArray, first, p - 1);
+    Sort(QuickSortArray, p + 1, end);
 }
 
-void QuickSort::display(int arr[], int n) //displays the array
+void QuickSort::display(int QuickSortArray[], int n) //displays the array
 {
     for (int i = 0; i < n; ++i)
     {
-        cout << arr[i] << " ";
+        cout << QuickSortArray[i] << " ";
         cout << "\n";
     }
     
