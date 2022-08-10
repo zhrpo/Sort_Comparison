cc33614993fbd08e8f10c8569e9935d516f52bf4
Tmenzie <98368403+Tmenzie@users.noreply.github.com>
2022-08-09 23:14:40 -0500

    Added Heap Sort

---
+++ Heap_Sort.cpp
@@ -0,0 +1,73 @@
+//Heap Sort Implimentation 
+#include <iostream>
+#include "Heap_Sort.h"
+using namespace std;
+
+
+void HSort::heap(int Heap[], int n, int i)
+{
+    int largest = i; //Tracks Largest INT
+    int L = 0; //Tracks LEFT
+    int R = 0; //Tracks RIGHT
+
+    // If left child is larger than root
+    if (L < n && Heap[L] > Heap[largest])
+    {
+        largest = 2 * i + 1;
+    }
+    // If right child is larger than largest so far
+    if (R < n && Heap[R] > Heap[largest])
+    {
+        largest = 2 * i + 2;
+    }
+    // If largest is not root
+    if (largest != i) {
+        swap(Heap[i], Heap[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heap(Heap, n, largest);
+    }
+}
+
+// main function to do heap sort
+void HSort::Sort(int Heap[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heap(Heap, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) 
+    {
+        // Move current root to end
+        swap(Heap[0], Heap[i]);
+
+        // call max heapify on the reduced heap
+        heap(Heap, i, 0);
+    }
+}
+
+/* A utility function to print array of size n */
+void HSort::printArray(int Heap[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        cout << Heap[i] << " ";
+        cout << "\n";
+    }
+}
+
+int main()
+{
+
+    HSort H;
+    int Heap[] = { 12, 11, 13, 5, 6, 7 };
+    int n = sizeof(Heap) / sizeof(Heap[0]);
+
+    H.Sort(Heap, n);
+
+    cout << "Sorted array is \n";
+    H.printArray(Heap, n);
+
+}
---
+++ Heap_Sort.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef HEAPSORT
+#define HEAPSORT
+
+class HSort
+{
+public:
+
+	void heap(int Heap[], int n, int i);
+	void Sort(int Heap[], int n);
+	void printArray(int Heap[], int n);
+
+};
+#endif
